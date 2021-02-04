5f1771e3b34a2f6a7778ca6c38f7b76be07f8e68
Arigam1 <yebkobvsebolshe@gmail.com>
2021-02-05 00:26:58 +0400

    pagination

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import { setUsersAC, unfollowAC, followAC } from '../../redux/users-reducer';
+import { setUsersAC, unfollowAC, followAC, setCurrentPageAC, setTotalUsersCountAC } from '../../redux/users-reducer';
 import Users from './Users';
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage,
     }
 }
   
@@ -19,7 +22,14 @@ let mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) =>{
             dispatch(setUsersAC(users))
-        }
+        },
+        setCurrentPage: (pageNumber) =>{
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount: (totalCount) =>{
+            dispatch(setTotalUsersCountAC(totalCount))
+        },
+
     }
 }
   
