dd7a7ef2bc632b599e5fba36f41dcef94b7094c5
Arigam1 <yebkobvsebolshe@gmail.com>
2021-02-07 00:51:20 +0400

    login!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import { Route } from "react-router-dom";
 import Music from "./components/Music/Music";
@@ -9,11 +8,12 @@ import News from "./components/News/News";
 import UsersContainer from "./components/Users/UsersContainer";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <Header />
+      <HeaderContainer />
       <Navbar />
       <div className="app-wrapper-content">
         <Route path="/dialogs" render={() => <DialogsContainer />} />
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,14 +1,16 @@
-import {combineReducers, createStore} from 'redux';
-import profileReducer from './profile-reducer';
-import sidebarReducer from './sidebar-reducer';
-import dialogsReducer from './dialogs-reducer';
-import usersReducer from './users-reducer';
+import { combineReducers, createStore } from "redux";
+import profileReducer from "./profile-reducer";
+import sidebarReducer from "./sidebar-reducer";
+import dialogsReducer from "./dialogs-reducer";
+import usersReducer from "./users-reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
-    dialogsPage: dialogsReducer,
-    profilePage: profileReducer,
-    sidebat: sidebarReducer,
-    usersPage: usersReducer,
+  dialogsPage: dialogsReducer,
+  profilePage: profileReducer,
+  sidebat: sidebarReducer,
+  usersPage: usersReducer,
+  auth: authReducer,
 });
 
 let store = createStore(reducers);
