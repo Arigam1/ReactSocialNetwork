6ad918c1f32b68a495c5bbfd04cbae7a3db4a94a
Arigam1 <yebkobvsebolshe@gmail.com>
2021-02-07 19:57:54 +0400

    UI BLL and new DAL! ! !

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -11,38 +11,29 @@ import {
 import * as axios from "axios";
 import Users from "./Users";
 import Preloader from "../common/Preloader/Preloader";
+import { usersAPI } from "../api/api";
 
 class UsersContainer extends React.Component {
   componentDidMount() {
     this.props.toggleIsFetching(true);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
-        {
-          withCredentials: true,
-        }
-      )
-      .then((response) => {
-        this.props.setUsers(response.data.items);
-        this.props.setTotalUsersCount(response.data.totalCount);
+    usersAPI
+      .getUsers(this.props.currentPage, this.props.pageSize)
+      .then((data) => {
+        debugger;
+        this.props.setUsers(data.items);
+        this.props.setTotalUsersCount(data.totalCount);
         this.props.toggleIsFetching(false);
       });
   }
 
-  onPageChanged = (pageNumbder) => {
+  onPageChanged = (pageNumber) => {
     this.props.toggleIsFetching(true);
-    this.props.setCurrentPage(pageNumbder);
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumbder}&count=${this.props.pageSize}`,
-        {
-          withCredentials: true,
-        }
-      )
-      .then((response) => {
-        this.props.setUsers(response.data.items);
-        this.props.toggleIsFetching(false);
-      });
+    this.props.setCurrentPage(pageNumber);
+
+    usersAPI.getUsers(pageNumber, this.props.pageSize).then((data) => {
+      this.props.setUsers(data.items);
+      this.props.toggleIsFetching(false);
+    });
   };
   render() {
     return (
---
+++ src/components/api/api.js
@@ -0,0 +1,21 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  headers: {
+    "API-KEY": "349d41a8-bede-4423-bc93-d8a5409e9908",
+  },
+});
+
+export const usersAPI = {
+  getUsers(currentPage, pageSize) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        return response.data;
+      });
+  },
+};
