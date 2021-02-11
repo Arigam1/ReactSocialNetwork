47d7fb3712cc8e3242cc79468c2cb7cf2259cc9b
Arigam1 <yebkobvsebolshe@gmail.com>
2021-02-11 16:37:26 +0400

    Redux-form (Login)

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,6 +5,7 @@ import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from "redux-form";
 
 let reducers = combineReducers({
   dialogsPage: dialogsReducer,
@@ -12,6 +13,7 @@ let reducers = combineReducers({
   sidebat: sidebarReducer,
   usersPage: usersReducer,
   auth: authReducer,
+  form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
